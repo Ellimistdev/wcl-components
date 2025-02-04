5deb8a63789264603c07f9af239f261e0f0c21c3
Ellimist <ellimistvox@gmail.com>
2025-02-04 14:02:40 -0800

    adds encounter enums

---
+++ definitions/encounterAbilities.ts
@@ -0,0 +1,82 @@
+export namespace EncounterAbilities {
+    export namespace Aberrus {
+        export enum Rashok {
+            ChargedSmash = 400777,
+            AncientFury = 405316,
+            SearingSlam = 405821,
+            Intermission = 406165,
+            ShadowlavaBlast = 406333
+        }
+
+        export enum Zskarn {
+            TacticalDestruction = 406678,
+            BlastWave = 403978,
+            Traps1 = 411188,
+            Traps2 = 405736,
+            Bomb = 406725,
+            UnstableEmbers = 404007
+        }
+
+        export enum Nymue {
+            FullBloom = 426775,
+            Unravel = 421368
+        }
+
+        export enum Tindral {
+            FallingStar = 420236,
+            MassEntanglement = 428695,
+            Supernova = 424140,
+            SeedOfFlame = 423265,
+            FieryGrowth = 424581,
+            Sunflame = 420240
+        }
+
+        export enum Sarkareth {
+            MassDisintegrate = 401642,
+            GlitteringSurge = 401819,
+            SearingBreath = 402051
+        }
+    }
+}
+
+// Companion type for the styling information
+export type AbilityStyle = {
+    color: string;
+    text: string;
+    verticalAlign?: "bottom";
+    y?: number;
+}
+
+export function createAbilityStyle(text: string, color: string, options?: Partial<AbilityStyle>): AbilityStyle {
+    return { color, text, ...options};
+}
+
+// Style information mapping
+export const AbilityStyles: Record<number, AbilityStyle> = {
+    [EncounterAbilities.Aberrus.Rashok.ChargedSmash]: createAbilityStyle('Charged Smash', '#F48CBA'),
+    [EncounterAbilities.Aberrus.Rashok.AncientFury]: createAbilityStyle('Ancient Fury', '#9E2B25'),
+    [EncounterAbilities.Aberrus.Rashok.SearingSlam]: createAbilityStyle('Searing Slam', '#D7CDCC'),
+    [EncounterAbilities.Aberrus.Rashok.Intermission]: createAbilityStyle('Intermission', '#5C7457'),
+    [EncounterAbilities.Aberrus.Rashok.ShadowlavaBlast]: createAbilityStyle('Shadowlava Blast', '#C69B6D'),
+
+    [EncounterAbilities.Aberrus.Zskarn.TacticalDestruction]: createAbilityStyle('Tactical Destruction', '#F3CA63', {verticalAlign: "bottom", y: 100}),
+    [EncounterAbilities.Aberrus.Zskarn.BlastWave]: createAbilityStyle('Blast Wave', '#A52422', {verticalAlign: "bottom", y: 100}),
+    [EncounterAbilities.Aberrus.Zskarn.Traps1]: createAbilityStyle('Traps 1', '#D7CDCC', {verticalAlign: "bottom", y: 100}),
+    [EncounterAbilities.Aberrus.Zskarn.Traps2]: createAbilityStyle('Traps 2', '#5C7457', {verticalAlign: "bottom", y: 100}),
+    [EncounterAbilities.Aberrus.Zskarn.Bomb]: createAbilityStyle('Bomb', '#C69B6D', {verticalAlign: "bottom", y: 100}),
+    [EncounterAbilities.Aberrus.Zskarn.UnstableEmbers]: createAbilityStyle('Unstable Embers', '#638CF2', {verticalAlign: "bottom", y: 100}),
+    
+    [EncounterAbilities.Aberrus.Sarkareth.MassDisintegrate]: createAbilityStyle('Mass Disintegrate', '#D7CDCC'),
+    [EncounterAbilities.Aberrus.Sarkareth.GlitteringSurge]: createAbilityStyle('Glittering Surge', '#F48CBA'),
+    [EncounterAbilities.Aberrus.Sarkareth.SearingBreath]: createAbilityStyle('Searing Breath', '#D7CDCC'),
+
+    [EncounterAbilities.Aberrus.Nymue.FullBloom]: createAbilityStyle('Full Bloom', '#D7CDCC'),
+    [EncounterAbilities.Aberrus.Nymue.Unravel]: createAbilityStyle('Unravel', '#F48CBA'),
+
+    [EncounterAbilities.Aberrus.Tindral.FallingStar]: createAbilityStyle('Falling Star', '#F3CA63'),
+    [EncounterAbilities.Aberrus.Tindral.MassEntanglement]: createAbilityStyle('Mass Entanglement', '#A52422'),
+    [EncounterAbilities.Aberrus.Tindral.Supernova]: createAbilityStyle('Supernova', '#D7CDCC'),
+    [EncounterAbilities.Aberrus.Tindral.SeedOfFlame]: createAbilityStyle('Seed of Flame', '#5C457'),
+    [EncounterAbilities.Aberrus.Tindral.FieryGrowth]: createAbilityStyle('Fiery Growth', '#C69B6D'),
+    [EncounterAbilities.Aberrus.Tindral.Sunflame]: createAbilityStyle('Sunflame', '#638CF2'),
+};
---
+++ definitions/encounterIds.ts
@@ -0,0 +1,19 @@
+export namespace EncounterIds {
+    export enum KhazAlgar {
+        KordacTheDormantProtector = 2997,
+        AggregationOfHorrors = 2988,
+        ShurraiAtrocityOfTheUndersea = 2994,
+        OrtsTheBrokenMountain = 2984,
+    }
+
+    export enum NerubarPalace {
+        UlgraxTheDevourer = 2902,
+        TheBloodboundHorror = 2917,
+        SikranCaptainOfTheSurekill = 2898,
+        Rashanan = 2918,
+        BroodtwisterOvinax = 2919,
+        NexusPrincessKyveza = 2920,
+        TheSilkenCourt = 2921,
+        QueenAnsurek = 2922
+    }
+}
